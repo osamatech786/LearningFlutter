4dc790d304a942adf3f98ad166c6a678c8ad9c5f
Osama <vactum0@gmail.com>
2021-11-10 08:14:28 +0500

    hello world

--- 2hello_world/lib/main.dart
+++ 2hello_world/lib/main.dart
@@ -1,1 +1,20 @@
+// ignore_for_file: prefer_const_constructors
 
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Text("Hellow World!"),
+      ),
+    );
+  }
+}
